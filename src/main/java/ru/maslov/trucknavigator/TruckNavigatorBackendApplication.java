77fee04d3aefc4c3cf5b04c106042cbb12207df9
danyamasloff <danyamasloff@yandex.ru>
2025-04-12 00:01:56 +0300

    Add local .gitignore

---
+++ src/main/java/ru/maslov/trucknavigator/TruckNavigatorBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.maslov.trucknavigator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckNavigatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckNavigatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/maslov/trucknavigator/TruckNavigatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maslov.trucknavigator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckNavigatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
