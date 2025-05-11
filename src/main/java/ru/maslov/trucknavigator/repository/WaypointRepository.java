98c044d906c5ff70b21ed0b1be4bc1333e5921c9
danyamasloff <danyamasloff@yandex.ru>
2025-05-11 21:28:05 +0300

    feat: добавление репозитория для Waypoint

--- src/main/java/ru/maslov/trucknavigator/repository/WaypointRepository.java
+++ src/main/java/ru/maslov/trucknavigator/repository/WaypointRepository.java
@@ -1,2 +1,33 @@
-package ru.maslov.trucknavigator.repository;public class WaypointRepository {
+package ru.maslov.trucknavigator.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import ru.maslov.trucknavigator.entity.Waypoint;
+
+import java.util.List;
+
+/**
+ * Репозиторий для работы с промежуточными точками маршрута.
+ */
+@Repository
+public interface WaypointRepository extends JpaRepository<Waypoint, Long> {
+
+    /**
+     * Находит все точки для указанного маршрута, отсортированные по порядковому номеру.
+     *
+     * @param routeId идентификатор маршрута
+     * @return список точек
+     */
+    List<Waypoint> findByRouteIdOrderByOrderIndex(Long routeId);
+
+    /**
+     * Удаляет все точки для указанного маршрута.
+     *
+     * @param routeId идентификатор маршрута
+     */
+    @Modifying
+    @Query("DELETE FROM Waypoint w WHERE w.route.id = :routeId")
+    void deleteByRouteId(Long routeId);
 }
